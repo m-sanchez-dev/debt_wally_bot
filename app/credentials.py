3ef64ee8bbdd44538fbb0397dfd1f091ae0e92bf
sWallyx <mikelsmartinez@aol.com>
2021-12-12 15:20:58 +0000

    Update URl

--- app/credentials.py
+++ app/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "5037113829:AAETtA_5J788tu1rcKX5LMkKrCxHjGG6vOU"
 bot_user_name = "debt_wally_bot"
-URL = "https://debt-wally-bot.herokuapp.com"
+URL = "https://debt-wally-bot.herokuapp.com/"
