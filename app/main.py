2c73f1e803682140542e55619fcccebf1acec170
sWallyx <mikelsmartinez@aol.com>
2021-12-12 15:25:27 +0000

    add another dummy route

--- app/main.py
+++ app/main.py
@@ -49,6 +49,25 @@ def set_webhook():
         return "webhook setup failed"
 
 
+@app.route("/", methods=["POST"])
+def another_route():
+        # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    dummy_message = 'something'
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode("utf-8").decode()
+    print("got text message :", text)
+
+    bot.sendMessage(chat_id=chat_id, text=dummy_message, reply_to_message_id=msg_id)
+    
+    return "ok"
+
+
 @app.route("/")
 def home_view():
     return "<h1>Welcome to Debt_bot</h1>"
